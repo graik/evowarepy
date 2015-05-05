7ead3bf297a3a8f268b2e210a0ffb8bd9541dfe3
graik <graik@users.noreply.github.com>
2015-05-05 14:58:31 +0200

    bump up version

--- evoware/__init__.py
+++ evoware/__init__.py
@@ -13,7 +13,7 @@
 ##   See the License for the specific language governing permissions and
 ##   limitations under the License.
 
-VERSION = (0,1,2)
+VERSION = (0,1,3)
 
 __version__ = '.'.join([ str(i) for i in VERSION])
 __VERSION__ = __version__
