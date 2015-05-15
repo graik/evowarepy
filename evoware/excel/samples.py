36fbf7c2e2ac7b35d9273627b2730ae2e048188d
graik <graik@users.noreply.github.com>
2015-05-15 15:06:56 +0200

    tentative Sample implementation

--- evoware/excel/samples.py
+++ evoware/excel/samples.py
@@ -20,14 +20,38 @@ from evoware import PlateFormat, PlateError
 
 class XlsSampleReader(XlsReader):
     HEADER_FIRST_VALUE = K.column_plate
+    
+class SampleError(Exception):
+    pass
 
 class Sample(object):
     """Representation of a single well / sample"""
     
-    def __init__(self, plate='', pos=0, plateformat=PlateFormat(96)):
-        self._plateid = plateid
-        self._plateformat = plateformat
-        self._pos = self._plateformat.pos2int(pos)
+    def __init__(self, id=None, subid=None, plate='', pos=0, 
+                 plateformat=PlateFormat(96), **kwargs):
+        self._id = ''
+        self._subid = ''
+        self._plateid = ''
+        self._plateformat = None
+        self._pos = 0
+        
+        ## initialize properties using setter methods
+        self.id = (id, subid)    
+        self.plateformat = plateformat
+        self.plateid = plate
+        self.position = pos
+
+    @property
+    def plateid(self):
+        """ID of plate or container holding this sample"""
+        return self._plateid
+    
+    @plateid.setter
+    def plateid(self, plate):
+        if plate is None:
+            plate = ''
+        assert type(plate) is str
+        self._plateid = plate.strip()
     
     def _setpos(self, pos):
         self._pos = self._plateformat.pos2int(pos)
@@ -49,7 +73,7 @@ class Sample(object):
        9
     """)
     
-    positionHuman = property(fget=_getposHuman, doc="""str, read-only property.
+    position2D = property(fget=_getposHuman, doc="""str, read-only property.
     'human readable' version of the well position. E.g. 'A1', 'B2', 'H12', etc. 
     """)
     
@@ -61,41 +85,55 @@ class Sample(object):
     
     plateformat = property(_getplateformat, _setplateformat)
     
+
+    def intfloat2int(self,x):
+        """convert floats like 1.0, 100.0, etc. to int *where applicable*"""
+        if type(x) is float:
+            if x % 1 == 0:
+                x = int(x)
+        return x
+
+    def _setid(self, ids):
+        """
+        Normalize input ID or (ID, sub-ID) tuple or ID#subID string into pair of
+        main ID and optional sub-ID.
+        @param ids: float or int or str or unicode or [float|int|str|unicode]
+        """
+        if type(ids) in [str, unicode] and '#' in ids:
+            ids = ids.split('#')
+
+        if type(ids) not in [tuple, list]:
+            ids = (ids,)
+
+        ids = [unicode(self.intfloat2int(x)).strip() for x in ids]
+        ids = [ x for x in ids if x ]  ## filter out empty strings but not '0'
+        
+        self._id = ids[0] if len(ids) > 0 else ''
+        self._subid = ids[1] if len(ids) > 1 else ''
+        
+    def _getid(self):
+        return self._id
     
+    def _getsubid(self):
+        return self._subid
+    def _setsubid(self, subid):
+        self._subid = subid if subid else ''
+    
+    def _getfullid(self):
+        if self._subid:
+            return '#'.join((self._id, self._subid))
+        return self._id
+    
+    id = property(fget=_getid, fset=_setid,
+                  doc='main sample ID (without sub-ID); or empty str')
+    subid = property(fget=_getsubid, fset=_setsubid, 
+                     doc='sub-ID if any; otherwise empty str')
+    fullid = property(fget=_getfullid, fset=_setid, 
+                      doc='complete ID which can be either ID or ID#subID')
+
 
 class SampleList(object):
     """
-    Extension of the basic Excel reader that is building a list of sample
-    locations.
-    
-    Assumes the following "parameters" defined before the actual sample table:
-    * reagent    <plate_ID>   <well>
-    (example 1: reagent   PCR-A   A2
-     example 2: reagent   PCR-A   2)
-    Optional: one or more plate format statements:
-    * format   <plate_ID>    <well number>
-    (example: format    PCR-A   96)
-    
-    Assumes the following columns in the sample table:
-    * plate ... plate ID (str or int)
-    * pos ... position (str or int)
-    Optional:
-    * volume ... volume in ul (int)    
-    
-    The order of these columns may be changed but HEADER_FIRST_VALUE then needs
-    to be adapted.
-    
-    Plate format statements are needed if well positions are given as 
-    "coordinates" such as A1, B2, etc. They are not needed if positions are
-    given as running (integer) numbers.
-        
-    Parser limitations
-    ===================
-    
-    As before, rows without value in the first column are silently ignored. 
-    Empty header columns (spacers without header name) are *not*
-    supported. A workaround is to give spacer columns a one-character header
-    such as '-' or '.'.    
     """
     
     def __init__(self, plateformat=PlateFormat(96), relaxedId=True):
@@ -106,19 +144,55 @@ class SampleList(object):
                               parts['Bba001'] may return parts['Bba001#a']
         """
         self.samples = []
-        self.plates = {'default':plateformat}
+        self.plateformats = {'default':plateformat}
         self.params = {}
-        
-        self.plateformat = plateformat
 
-    def getSampleReaderClass(self, fname):
-        return XlsSampleReader
-    
-    def readSamples(self, fname):
-        R = self.getSampleReaderClass(fname)
-        reader = R(plateformat=self.plateformat)
-        reader.read(fname)
+    def clean2str(self, x):
+        """convert integer floats to int (if applicable), then strip to unicode"""
+        x = self.intfloat2int(x)
+
+        if type(x) is not unicode:
+            x = unicode(x)
+
+        x = x.strip()
+        return x
+
+    def cleanEntry(self, d):
+        """convert and clean single sample dictionary (in place)"""
+        for key, value in d.items():
+            d[key] = self.clean2str(value)
+
+
+    def addraw(self, d):
+        """create new entry from dictionary"""
+        pass
         
-        for d in reader.rows:
-            pass
+
+######################
+### Module testing ###
+from evoware import testing
+
+class Test(testing.AutoTest):
+    """Test Sample"""
+
+    TAGS = [ testing.NORMAL ]
+
+    def test_sample(self):
+        s = Sample(id='BBa1000', subid=1.0, plate='plateA', pos='A1')
+        self.assertEqual(s.id, 'BBa1000')
+        self.assertEqual(s.subid, '1')
+        self.assertEqual(s.fullid, 'BBa1000#1')
+        self.assertEqual(s.position, 1)
+        self.assertEqual(s.plateid, 'plateA')
+        self.assertEqual(s.position2D, 'A1')
+        
+        s.id = 'BBa2000'
+        self.assertEqual(s.id, 'BBa2000')
+        self.assertEqual(s.subid, '')
+        self.assertEqual(s.fullid, 'BBa2000')
+        
+        s.id = 'BBa3000#a'
+        self.assertEqual(s.id, 'BBa3000')
+        self.assertEqual(s.subid, 'a')
+        
         
