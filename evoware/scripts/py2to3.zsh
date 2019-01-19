68db919364eef5f7afb5e628262422f00d5088f4
graik <graik@users.noreply.github.com>
2019-01-19 13:27:50 +0300

    small script for file-by-file conversion

---
+++ evoware/scripts/py2to3.zsh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "USAGE: "
+echo "py2to3.zsh module.py"
+echo "Run from package folder evowarepy/evoware"
+echo "This will change the file in-place and then try to run a graphical diff"
+echo "against a copy in another folder (given as second argument)"
+
+OLDPATH="../evowarepy2x/evoware"
+
+exists() { type -t "$1" > /dev/null 2>&1; }
+
+2to3 -n -W $1
+
+if exists meld; then
+    meld $1 $OLDPATH/$1
+elif exists bcomp; then
+    bcomp $1 $OLDPATH/$1
+fi
